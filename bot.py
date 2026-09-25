0a1455ec2afb165c6cfbb816437cfcc52ad78039
usadevi7099-afk <usadevi7099@gmail.com>
2026-09-25 13:06:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -474,7 +474,7 @@ async def log_primary_purchase(uid, country, price, amount, year, qty):
     try:
         t = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         msg = (f"{PE_LIGHTNING} <b>NEW PURCHASE SUCCESSFUL</b>\n\n"
-               f"{P_ID} Order ID: <code>{order_id}</code>\n\n"
+               
                f'{P_ID} Uꜱᴇʀ Iᴅ: <code>*****{str(uid)[-5:]}</code>\n'
                f"{P_GLOBE} Cᴏᴜɴᴛʀʏ: {country}\n"
                f"{P_MONEY} Pʀɪᴄᴇ: {P_INR}{price}\n"
